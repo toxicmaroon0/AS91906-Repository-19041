958c652b0f79741f6158debe8fa67dec20a5d4bd
Eli Spice <toxicmaroon0@gmail.com>
2023-06-16 09:45:05 +1200

    Add comments, more logic

--- py_assessment/py_assessment.py
+++ py_assessment/py_assessment.py
@@ -18,7 +18,7 @@ class Calculator():
         buttons = [
             "(", ")", "mc", "m+", "m-", "mr", "AC", "±", "%", "÷",
             "2{}".format(self.get_super('nd')), "x{}".format(self.get_super('2')), "x{}".format(self.get_super('3')), "x{}".format(self.get_super('y')), "e{}".format(self.get_super('x')), "10{}".format(self.get_super('x')), "7", "8", "9", "×",
-            "{}/x".format(self.get_super('1')), "{}√{}".format(self.get_super('2'),self.get_sub('x')), "{}√{}".format(self.get_super('3'),self.get_sub('x')), "{}√{}".format(self.get_super('y'),self.get_sub('x')), "ln", "log{}".format(self.get_sub('10')), "4", "5", "6", "-",
+            "{}/x".format(self.get_super('1')), "{}√x".format(self.get_super('2')), "{}√x".format(self.get_super('3')), "{}√x".format(self.get_super('y')), "ln", "log{}".format(self.get_sub('10')), "4", "5", "6", "-",
             "x!", "sin", "cos", "tan", "e", "EE", "1", "2", "3", "+",
             "Rad", "sinh", "cosh", "tanh", "π", "Rand", "0", ".", "="
         ]
@@ -26,13 +26,20 @@ class Calculator():
         row = 1
         col = 0
         for button in buttons:
-            btn = tk.Button(root, text=button, font=("Helvetica", 16), width=1, height=1)
-            btn.grid(row=row, column=col, padx=5, pady=5, sticky="NSEW")
+            btn = tk.Button(background, text=button, font=("Helvetica", 16), width=1, height=1)
+            btn.grid(row=row, column=col, sticky="NSEW")
+            if button == "0":
+                btn.grid_configure(columnspan=2)
+                col += 1
             btn.bind("<Button-1>", self.button_click)
             col += 1
-            if col > 10:
+            if col > 9:
                 col = 0
                 row += 1
+                
+        # Entry Field
+        self.entry_field = tk.Entry(root,font=("Helvetica", 20), justify="right")
+        self.entry_field.grid(row=0, column=0, columnspan=10,sticky="NSEW")
 
     def button_click(self, event):
         button = event.widget
@@ -40,13 +47,23 @@ class Calculator():
 
         if text == "=":
             try:
-                result = eval(self.entry_field.get())
+                entry_content = self.entry_field.get()
+                entry_content = entry_content.replace('×', '*')
+                entry_content = entry_content.replace('÷', '/')
+                entry_content = entry_content.replace('%', '/100')
+                result = eval(entry_content)
                 self.entry_field.delete(0, tk.END)
                 self.entry_field.insert(tk.END, str(result))
             except:
                 messagebox.showerror("Error", "Invalid Expression")
         else:
             self.entry_field.insert(tk.END, text)
+        
+        if text == "AC":
+            try:
+                self.entry_field.delete(0, tk.END)
+            except:
+                messagebox.showerror("Error", "Could not Clear")
 
 
     #Text Formatting
@@ -63,14 +80,18 @@ class Calculator():
         return x.translate(res)
         
         
-
+#Define Window name
 root = tk.Tk()
-background = tk.Canvas(root, width=575,height=270)
-background.grid()
+#Create Canvas
+background = tk.Canvas(root, width=575,height=300)
+background.grid(row=1,column=0,columnspan=10,rowspan=5,sticky="NSEW")
+#Call class logic and buttons
 Calculator()
+#Configure window
 root.title("Calculator")
 root.geometry("575x320")
 root.wm_attributes('-alpha', 0.92)
 root.configure(background='lightgrey')
 root.resizable(False,False)
+#Refresh window
 root.mainloop()
